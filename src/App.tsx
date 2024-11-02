cc32cadf4c5f1c4c042237372e56e104ed510cea
Sai Akhil Katukam <saiakhilk.katukam@gmail.com>
2024-11-02 19:56:26 +0530

    small change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import DynamicCSVTodoList from "./components/dynamic";
 
 export default function App() {
   return (
-    <div className="bg-blue-500 h-screen p-2 flex flex-col">
+    <div className="bg-blue-500 flex-1 h-screen p-2 flex flex-col">
       <DynamicCSVTodoList />
     </div>
   );
